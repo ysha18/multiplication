71201103d2e3a1fbbd05743bc5cfbe38b6e6de6b
Harold Yeo <hyeo@guidewire.com>
2022-04-11 23:27:24 -0500

    Added spring data + UI files

---
+++ src/main/resources/static/precache-manifest.eabf16e15b1efc670409e6e925808ae3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd1ca425f112358bf5750cedfdcbadd0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebc0ecd46158f15dbf2c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "115a4a2f067f926bb633",
+    "url": "/static/js/2.548ff48a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.548ff48a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebc0ecd46158f15dbf2c",
+    "url": "/static/js/main.29b05c46.chunk.js"
+  },
+  {
+    "revision": "304a5910f4c9f00de56b",
+    "url": "/static/js/runtime-main.8b342bfc.js"
+  }
+]);
